1d24cc1c7bb33ae97adb4479c057291151c72025
Akos Szakaly <akos.szakaly@gmail.com>
2016-11-04 21:09:44 +0100

    final beautified javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*Getting the data from mashape API*/
 
-function getQuote(){
+function getQuote() {
     $.ajax({
         headers: {
             'X-Mashape-Key': 'YA1YlFUqzfmshrFQ4tU39TknJ6l2p1ASZiSjsn9MzW4WwXXXTr',
@@ -9,29 +9,33 @@ function getQuote(){
         },
         url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
         method: 'POST',
-        success: function (response){
-            var dataj=JSON.parse(response);
-            var quoteText=dataj.quote;
-            var author=dataj.author;
+        success: function (response) {
+            var dataj = JSON.parse(response);
+            var quoteText = dataj.quote;
+            var author = dataj.author;
 
-            document.getElementById("quote").innerHTML=quoteText;
-            document.getElementById("author").innerHTML=author;
+            document.getElementById("quote").innerHTML = quoteText;
+            document.getElementById("author").innerHTML = author;
         }
     });
 }
 
 
-function tweet (){
-    var quote=document.getElementById("quote").innerHTML;
-    var author=document.getElementById("author").innerHTML;
-    var link="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent('"' + quote + '" ' + author);
+function tweet() {
+    var quote = document.getElementById("quote").innerHTML;
+    var author = document.getElementById("author").innerHTML;
+    var link = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent('"' + quote + '" ' + author);
 
-    window.open(link,'_blank','height=350,width=650');
+    window.open(link, '_blank', 'height=350,width=650');
 }
 
 
-$(document).ready (function (){
-    $('#generateQuote').click(function() {getQuote()});
-    $('#tweet').click(function() {tweet()});
+$(document).ready(function () {
+    $('#generateQuote').click(function () {
+        getQuote()
+    });
+    $('#tweet').click(function () {
+        tweet()
+    });
 
 })
